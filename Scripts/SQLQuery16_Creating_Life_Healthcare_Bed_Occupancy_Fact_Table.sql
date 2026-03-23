ebc937b9674c57b13d44d985e75e3301140074c8
Andile Dube <andile.data99@gmail.com>
2026-03-23 14:55:16 +0200

    pushing life healthcare bed occupancy table creation code using the terminal

---
+++ Scripts/SQLQuery16_Creating_Life_Healthcare_Bed_Occupancy_Fact_Table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Fact_Life_Healthcare_Bed_Occupancy (
+    Bed_ID INT ,
+    Patient_ID INT,
+    Bed_Type_ID INT ,
+    Location_ID INT,
+    Date_ID INT ,
+    Admission_Type_ID INT ,
+    Procedure_Type_ID INT ,
+    Admission_Diagnosis_ID INT ,
+    Occupied_Beds INT ,
+    Available_Beds INT ,
+    Unoccupied_Beds INT ,
+    Admission_Count INT ,
+    Discharge_Count INT ,
+    Occupancy_Rate INT ,
+    Procedure_Length INT 
+);
